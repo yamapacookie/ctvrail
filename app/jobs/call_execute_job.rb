1cb63450d8cbfa3f44ddf671ae9871fb1f9e4915
yamapa <coockiefortheworld@gmail.com>
2022-12-11 01:35:19 +0900

    dockerfile test

--- app/jobs/call_execute_job.rb
+++ app/jobs/call_execute_job.rb
@@ -1,5 +1,7 @@
 class CallExecuteJob < ApplicationJob
     def perform
+      require 'selenium-webdriver'
+
       testdata = Clist.find(1)[:title]
       puts testdata
       puts 'congraturation regular execution!!'
